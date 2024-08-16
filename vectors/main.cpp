194be2f17fd833619d8c6850d6635c453ecd1175
DizzyWaters <mkulvanovskis@gmail.com>
2024-08-16 17:46:54 +0300

    working on calc

--- vectors/main.cpp
+++ vectors/main.cpp
@@ -1,14 +1,93 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
+// ----------------------------------------------------------------------
+// in
+const std::string MyEmptyString = "No input";
+
+class Calculator 
+{
+private:
+
+	int a = 0;
+	int b = 0;
+	int currentValue = 0;
 
 
-void PrintVector(std::vector <int> a) 
+public:
+
+	void SetA(int a)
+	{
+		this->a = a;
+	}
+	void SetB(int b) 
+	{
+		this->b = b;
+	}
+	int GetA() 
+	{
+		return this->a;
+	}
+	int GetB() 
+	{
+		return this->b;
+	}
+	void GetAdress()
+	{
+		std::cout << "baa: " << this << std::endl;
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+void PrintINT(std::string comment, int a) {
+
+	if (comment.empty()) {
+		std::cout << MyEmptyString << " : " << a << std::endl;
+	}
+	else
+	std::cout << comment << " : " << a << std::endl;
+};
+
+void PrintINT(std::string comment, const int *a) {
+
+	if (comment.empty()) {
+		std::cout << MyEmptyString << " : " << a << std::endl;
+	}
+	else
+		std::cout << comment << " : " << a << std::endl;
+};
+
+
+void PrintINT(const int a) 
+{
+	std::cout << MyEmptyString << " : " << a << std::endl;
+};
+
+void PrintINT(const int *a)
+{
+	std::cout << MyEmptyString << " : " << a << std::endl;
+};
+template <typename T>
+
+void PrintVector(std::vector <T> a) 
 {
 	for (auto var : a)
 	{
-		std::cout << var << std::endl;
+		std::cout << var << " ";
 	}
-}
+	std:: cout << " " << std::endl;
+	std::cout << "End of print..." << std::endl;
+} 
 
 void PrintVectorAddress(const std::vector <int> &a)
 {
@@ -29,18 +108,43 @@ void AddValuesToVector(std::vector <int>& a, int input)
 	}
 }
 
+std::vector<long long> pascalsTriangle(int input)
+{
+	std::vector <long long> result;
+	std::vector <long long> tempV;
+	int C = 1;
+	for (int i = 1; i <= input; ++i) {
+		C = 1;
+		for (int j = 1; j <= i; ++j) {
+			tempV.push_back(C);
+			C = C * (i - j) / j;
+		}
+		result.insert(result.end(), tempV.begin(), tempV.end());
+	}
+	return result;
+}
+
 int main()
 {
-	std::vector <int> vec = { 1,2,3,4,5,6 };
-	
-	PrintVector(vec);
-	PrintVectorAddress(vec);
-	PrintVectorAddress(vec);
+    /*
+	int test = 25;
+	int *testPT = &(test);
+	PrintINT("test", test);
+	PrintINT("pointer &test", &test);
+	PrintINT("pointer *testPT = &(test)", testPT);
+	*/
 
-	AddValuesToVector(vec, 7);
+	int a = 0,
+		b = 0, c = 0;
 
-	PrintVector(vec);
-	PrintVectorAddress(vec);
+
+	std::cout << "CALCULATOR!!!" << std::endl;
+	std::cout << "Input value: ";
+	std::cin >> a;
+
+
+
+
 
 	return 0;
 }
